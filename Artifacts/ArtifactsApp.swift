46fafda29f40da80e384826d5c5733fe38016edb
RyanEAA <aparicioryan2003@gmail.com>
2025-09-09 14:13:14 -0500

    Initial Commit

---
+++ Artifacts/ArtifactsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ArtifactsApp.swift
+//  Artifacts
+//
+//  Created by Ryan Aparicio on 9/9/25.
+//
+
+import SwiftUI
+
+@main
+struct ArtifactsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
