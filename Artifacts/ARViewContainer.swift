8814a6072f3c08357da9f5f732487a375bf73a89
RyanEAA <aparicioryan2003@gmail.com>
2025-09-09 22:39:16 -0500

    started laying foundation for the rest of the AR project

---
+++ Artifacts/ARViewContainer.swift
@@ -0,0 +1,18 @@
+//
+//  ARViewContainer.swift
+//  Artifacts
+//
+//  Created by Ryan Aparicio on 9/9/25.
+//
+
+import SwiftUI
+
+struct ARViewContainer: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ARViewContainer()
+}
---
+++ Artifacts/SheetView.swift
@@ -0,0 +1,18 @@
+//
+//  SheetView.swift
+//  Artifacts
+//
+//  Created by Ryan Aparicio on 9/9/25.
+//
+
+import SwiftUI
+
+struct SheetView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SheetView()
+}
