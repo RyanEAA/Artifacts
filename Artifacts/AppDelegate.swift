b90b0d444251d248b26230f5700fb1ee6a3b19b8
RyanEAA <aparicioryan2003@gmail.com>
2025-09-17 15:56:42 -0500

    finished implementing firebase auth to the app. Added a login screen that leads to a profile view that users can then user to go to the current ARView

---
+++ Artifacts/AppDelegate.swift
@@ -0,0 +1,8 @@
+//
+//  AppDelegate.swift
+//  Artifacts
+//
+//  Created by Ryan Aparicio on 9/17/25.
+//
+
+import Foundation
---
+++ Artifacts/HomeARView.swift
@@ -0,0 +1,18 @@
+//
+//  HomeARView.swift
+//  Artifacts
+//
+//  Created by Ryan Aparicio on 9/17/25.
+//
+
+import SwiftUI
+
+struct HomeARView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HomeARView()
+}
