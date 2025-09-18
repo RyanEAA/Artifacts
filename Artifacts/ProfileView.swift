6491c64278459cc2ce57606d5abde02d1de1dbe4
RyanEAA <aparicioryan2003@gmail.com>
2025-09-18 00:14:09 -0500

    added ar button to profile view. will remove redundant code in the future

--- Artifacts/ProfileView.swift
+++ Artifacts/ProfileView.swift
@@ -9,14 +9,34 @@ import SwiftUI
 
 struct ProfileView: View {
     @EnvironmentObject var session: SessionManager
+    @State var isPresented: Bool = false
+
 
     var body: some View {
         VStack(spacing: 20) {
+            // welcome user
             if let user = session.user {
                 Text("Welcome, \(user.email ?? "User")!")
                     .font(.title)
             }
-
+            
+//            Enter AR Mode
+            Image("toyplane_img")
+                .resizable()
+                .scaledToFit()
+                .foregroundStyle(.tint)
+                .clipShape(RoundedRectangle(cornerSize: CGSize(width: 20, height: 20)))
+                .padding(24)
+            
+            Button {
+                isPresented.toggle()
+            } label: {
+                Label("View in AR", systemImage: "arkit")
+            }.buttonStyle(BorderedProminentButtonStyle())
+                .padding(24)
+            
+            
+            // sign out button
             Button("Sign Out") {
                 session.signOut()
             }
@@ -27,6 +47,9 @@ struct ProfileView: View {
             .cornerRadius(8)
         }
         .padding()
+        .fullScreenCover(isPresented: $isPresented, content: {
+           SheetView(isPresented: $isPresented)
+        })
     }
 }
 #Preview {
