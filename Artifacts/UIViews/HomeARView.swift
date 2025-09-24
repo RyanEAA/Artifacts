78c8e029cffb7efe0cf5d417b915da5f6cf1f49a
eligulley1 <eligulley@gmail.com>
2025-09-24 13:57:48 -0500

    New form of "Annotations"

--- Artifacts/UIViews/HomeARView.swift
+++ Artifacts/UIViews/HomeARView.swift
@@ -1,36 +1,10 @@
-//
-//  SheetView.swift
-//  Artifacts
-//
-//  Created by Ryan Aparicio on 9/9/25.
-//
-
-import SwiftUI
-
 import SwiftUI
 
 struct HomeARView: View {
-//    @State private var modelName: String = "toy_biplane_realistic"
-    @State private var modelName: String = "minecraft_sign"
-    @State private var touchLocation: CGPoint? = nil
-    @State private var placeRequested: Bool = false
-
     var body: some View {
         ZStack {
-            // Pass bindings to the representable
-            ARViewContainer(modelName: $modelName,
-                            touchLocation: $touchLocation,
-                            placeRequested: $placeRequested)
-                .gesture(
-                    DragGesture(minimumDistance: 0, coordinateSpace: .local)
-                        .onEnded { value in
-                            touchLocation = value.location
-                            placeRequested = true
-                        }
-
-                )
+            ARViewContainer()
                 .ignoresSafeArea()
-
         }
     }
 }
