9103d4303d65a5f044160cd7f2f026a3cddd8791
Swapnil Puri <swappypuri@gmail.com>
2026-02-27 19:24:51 -0600

    Fixed profile image selector, app icon, and launch screen.

---
+++ Artifacts/UIViews/ImagePicker.swift
@@ -0,0 +1,18 @@
+//
+//  ImagePicker.swift
+//  Artifacts
+//
+//  Created by Swapnil Puri on 2/27/26.
+//
+
+import SwiftUI
+
+struct ImagePicker: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ImagePicker()
+}
