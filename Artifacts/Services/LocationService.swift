5021595c24a99c15ba072978ed41d3a3f4b17961
Swapnil Puri <swappypuri@gmail.com>
2026-03-01 18:04:14 -0600

    Enabled map to showcase artifact location and interfacing with Apple Maps

---
+++ Artifacts/Services/LocationService.swift
@@ -0,0 +1,18 @@
+//
+//  LocationService.swift
+//  Artifacts
+//
+//  Created by Swapnil Puri on 3/1/26.
+//
+
+import SwiftUI
+
+struct LocationService: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LocationService()
+}
